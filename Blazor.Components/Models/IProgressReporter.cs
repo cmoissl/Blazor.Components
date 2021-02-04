1e2134e95736e596da91e7d50454af8249dabd8f
Christoph Moissl <mailto:christoph_moissl@external.mckinsey.com>
2021-02-04 09:42:28 +0100

    initial verison

---
+++ Blazor.Components/Models/IProgressReporter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMoissl.Blazor.Components.Models
+{
+    public interface IProgressReporter : IProgress<ProgressMessage>
+    {
+        void Report(ProgressMessage value);
+        Task ReportAsync(ProgressMessage value);
+    }
+}
---
+++ Blazor.Components/Models/ProgressMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMoissl.Blazor.Components.Models
+{
+    public class ProgressMessage
+    {
+        public bool IsBusy { get; set; }
+        public int ProgressInPercent { get; set; }
+        public string Message { get; set; }
+    }
+}
